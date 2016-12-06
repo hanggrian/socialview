2cd6a1937d800db1e9407168afa78f39af280302
Hendra Anggrian <hendraanggrian@gmail.com>
2016-12-06 23:55:43 +0700

    become 'socialautocompletetextview'

---
+++ socialautocompletetextview/src/main/java/io/github/hendraanggrian/socialautocompletetextview/ClickableForegroundColorSpan.java
@@ -0,0 +1,40 @@
+package io.github.hendraanggrian.socialautocompletetextview;
+
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import rx.Observable;
+
+public final class ClickableForegroundColorSpan extends ClickableSpan {
+
+    private final int color;
+    private final SocialAutoCompleteTextView.OnSocialClickListener listener;
+
+    public ClickableForegroundColorSpan(@ColorInt int color, @NonNull SocialAutoCompleteTextView.OnSocialClickListener listener) {
+        this.color = color;
+        this.listener = listener;
+    }
+
+    @Override
+    public void updateDrawState(TextPaint textPaint) {
+        textPaint.setColor(color);
+    }
+
+    @Override
+    public void onClick(View widget) {
+        Observable.just(widget)
+                .map(view -> (TextView) view)
+                .map(TextView::getText)
+                .map(charSequence -> (Spanned) charSequence)
+                .subscribe(spanned -> {
+                    int start = spanned.getSpanStart(ClickableForegroundColorSpan.this);
+                    int end = spanned.getSpanEnd(ClickableForegroundColorSpan.this);
+                    listener.onClick(spanned.subSequence(start + 1/*skip "#" sign*/, end).toString());
+                });
+    }
+}
