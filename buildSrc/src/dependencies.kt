a1d7d2b73cb167a8a9d2d66745fb485a2f6a664b
Hendra Anggrian <hendraanggrian@gmail.com>
2019-06-22 15:19:21 +0700

    Gradle 5.4.1

--- buildSrc/src/dependencies.kt
+++ buildSrc/src/dependencies.kt
@@ -12,10 +12,10 @@ fun DependencyHandler.androidx(
 
 fun DependencyHandler.material() = "com.google.android.material:material:$VERSION_ANDROIDX"
 
-fun DependencyHandler.picasso(): String = "com.squareup.picasso:picasso:2.71828"
+fun DependencyHandler.picasso(): String = "com.squareup.picasso:picasso:$VERSION_PICASSO"
 
-fun DependencyHandler.bintray() = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4"
+fun DependencyHandler.bintray() = "com.jfrog.bintray.gradle:gradle-bintray-plugin:$VERSION_BINTRAY"
 inline val PluginDependenciesSpec.bintray get() = id("com.jfrog.bintray")
 
-fun DependencyHandler.bintrayRelease() = "com.novoda:bintray-release:0.9"
+fun DependencyHandler.bintrayRelease() = "com.novoda:bintray-release:$VERSION_BINTRAY_RELEASE"
 inline val PluginDependenciesSpec.`bintray-release` get() = id("com.novoda.bintray-release")
